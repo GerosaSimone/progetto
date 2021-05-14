7f0a1321366659781a9672c5f90c9a60ba4c09b8
Locatelliandrea <locatelliandrea81@gmail.com>
2021-05-14 22:33:48 +0200

    venerdi sera

--- sito/javascript.js
+++ sito/javascript.js
@@ -1,21 +1,58 @@
+
+/*
+function WriteToFile() {
+
+    // se il file esiste aggiunge due righe
+    // con il contenuto delle variabili nome/cognome
+    if (fso.FileExists(filename)) {
+        var a, ForAppending, file;
+        ForAppending = 8;
+        file = fso.OpenTextFile(filename, ForAppending, false);
+        file.WriteLine(nome);
+        file.WriteLine(password);
+    }
+    //Se non esiste crea il file e
+    // aggiunge due righe
+    // con il contenuto delle variabili nome/cognome
+    else {
+        
+        file.WriteLine(cognome);
+        file.WriteLine(cognome);
+    }
+    file.Close();
+}
+*/
+
 function register() {
+    var filename = "C:\\Users\\locat\\Desktop\\sito\\database.txt";
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
     if (controllo) {
-        var utente = utente(document.getElementById("username").textContent, document.getElementById("password").textContent);
+        let ut = utente(document.getElementById("username").value, document.getElementById("password").value);
 
         if (esistente) {
             document.getElementById("err").innerHTML == "l'utente esiste gia, fare log-in";
         } else {
-            //aggiungi al file l'utente
-            clear();
+            
+            if (fso.FileExists(filename)) {
+                var a, ForAppending, file;
+                ForAppending = 8;
+                file = fso.OpenTextFile(filename, ForAppending, false);
+                file.WriteLine(ut.to_string());
+            }
+            utenti.push(ut);
+       
+           clear();
         }
     } else {
         document.getElementById("err").innerHTML == "inserire tutti i campi";
+        
     }
+    
 }
 function login() {
     if (controllo) {
-        var ut = utente(document.getElementById("username").textContent, document.getElementById("password").textContent);
-//controllo che l'utente esista nel file
+        let ut = utente(document.getElementById("username").textContent, document.getElementById("password").textContent);
+        //controllo che l'utente esista nel file
         if (esistente(ut)) {
             //passare alla pagina del download
         } else {
@@ -27,18 +64,25 @@ function login() {
 
 }
 function clear() {
-    document.getElementById("username").textContent == "";
-    document.getElementById("password").textContent == "";
+    document.getElementById("username").value == "";
+    document.getElementById("password").value == "";
 }
 function vettoreUtenti() {
 
-    return Object;//ritorna un vettore con tutti gli utenti esistenti
+
+    return Object;
 }
-function esistente(utente) {
-//controllo se l utente esiste nel vettore di utenti
+function esistente(ut) {
+    //controllo se l utente esiste nel vettore di utenti
+    for (let i; i < utenti.lenght(); i++) {
+        if (utenti[i] == ut)
+            return true;
+        else
+            return false;   
+    }
 }
 function controllo() {
-    if ((document.getElementById("username").textContent == "") || (document.getElementById("password").textContent == "")) {
+    if ((document.getElementById("username").value == "") || (document.getElementById("password").value == "")) {
         return false;
     } else {
         return true;
@@ -46,6 +90,7 @@ function controllo() {
 }
 
 class utente {
+
     utente(nome, password) {
         this.nome = nome;
         this.password = password;
@@ -59,9 +104,10 @@ class utente {
     }
 
     to_string() {
-        return this.nome + "-" + this.password;
+        return this.nome + "-" + this.password + ";";
     }
 
+    utenti = new Array();
 
 
 }
